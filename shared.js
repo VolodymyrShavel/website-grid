debf42f43928eff159f8f10767d0e0285b6681aa
Volodia <volodiasyd@gmail.com>
2022-10-26 20:05:01 +0300

    fixed display on backdrop

--- shared.js
+++ shared.js
@@ -1,13 +1,19 @@
-let backdrop = document.querySelector('.backdrop');
-let modal = document.querySelector('.modal');
-let noButton = document.querySelector('.modal__action--negative');
-let selectPlanButton = document.querySelectorAll('.plan button');
-let toggleNavButton = document.querySelector('.toggle-button');
-let mobileNav = document.querySelector('.mobile-nav');
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton = document.querySelector('.main-nav__item--cta');
+
+// console.dir(backdrop.style['background-image']);
 
 // console.dir(backdrop);
-for (let i = 0; i < selectPlanButton.length; i++) {
-   selectPlanButton[i].addEventListener('click', function () {
+for (var i = 0; i < selectPlanButtons.length; i++) {
+   selectPlanButtons[i].addEventListener('click', function () {
+      // modal.style.display = "block";
+      // backdrop.style.display = "block";
+      // modal.className = 'open'; // This will actually overwrite the complete class list
       modal.classList.add('open');
       backdrop.style.display = 'block';
       setTimeout(function () {
@@ -17,11 +23,18 @@ for (let i = 0; i < selectPlanButton.length; i++) {
 }
 
 backdrop.addEventListener('click', function () {
+   // mobileNav.style.display = 'none';
    mobileNav.classList.remove('open');
    closeModal();
 });
 
+if (modalNoButton) {
+   modalNoButton.addEventListener('click', closeModal);
+}
+
 function closeModal() {
+   //   backdrop.style.display = "none";
+   //   modal.style.display = "none";
    if (modal) {
       modal.classList.remove('open');
    }
@@ -30,11 +43,13 @@ function closeModal() {
       backdrop.style.display = 'none';
    }, 200);
 }
-noButton.addEventListener('click', closeModal);
 
-function openModal() {
+toggleButton.addEventListener('click', function () {
+   // mobileNav.style.display = 'block';
+   // backdrop.style.display = 'block';
    mobileNav.classList.add('open');
-   backdrop.classList.add('open');
-}
-
-toggleNavButton.addEventListener('click', openModal);
+   backdrop.style.display = 'block';
+   setTimeout(function () {
+      backdrop.classList.add('open');
+   }, 10);
+});
